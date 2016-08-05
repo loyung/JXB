d1dd742e52f1fe96ebf0b3b5ce797ef1b8d665b7
liuziyang <mloyung@hotmail.com>
2016-08-05 17:27:59 +0800

    新增Api

---
+++ JXB_WEB/JXB/Areas/API/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using JXB.PublicMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JXB.Areas.API.Controllers
+{
+    public class HomeController : BaseController
+    {
+        //
+        // GET: /API/Home/
+        public ActionResult Index(string Name)
+        {
+            return View((object)Name);
+        }
+
+        public ActionResult Error(string Msg)
+        {
+            return ReturnJsonApi(Msg, null);
+        }
+	}
+}
---
+++ JXB_WEB/JXB/Areas/API/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JXB.Areas.API.Controllers
+{
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// aaaa
+        /// </summary>
+        /// <param name="aa">aaa</param>
+        /// <returns>ccc</returns>
+        public ActionResult Index(int? aa)
+        {
+            return View();
+        }
+	}
+}
